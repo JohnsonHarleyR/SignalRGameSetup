d9cf0fdf232fc778312fd37d098dd38d4040e69a
Harley Johnson <hjohnson@uwm.com>
2021-08-17 18:57:58 -0400

    The squares can now be highlighted on a board in order to choose where to place ships. It just needs to be able to set it now.

---
+++ SignalRGameSetup/Scripts/board.es5.js
@@ -0,0 +1,262 @@
+﻿// Hub
+
+// Go to the game page
+'use strict';
+
+function goToGame() {
+    connection.server.goToGame({
+        gameCode: gameSetup.GameCode,
+        participantId: clientId
+    });
+}
+
+function updateEnterGameButton() {
+    // only let them use or see this button if they started the game
+    if (isStarter) {
+        // only let it be available if there are enough players for the game
+        if (gameSetup.Players.length < minimumPlayers) {
+
+            startGameBtn.disabled = true;
+            var howManyLeft = minimumPlayers - gameSetup.Players.length;
+            if (howManyLeft === 1) {
+                startGameBtn.innerHTML = 'Only ' + howManyLeft + ' player needed to start';
+            } else {
+                startGameBtn.innerHTML = 'only ' + howManyLeft + ' players needed to start';
+            }
+        } else {
+            startGameBtn.disabled = false;
+            startGameBtn.innerHTML = 'Start Game';
+        }
+    } else {
+        startGameBtn.style.display = 'none';
+    }
+}
+
+// Creating a new room
+function newRoom(allowAudienceChoice) {
+    console.log("New room.");
+    clientName = enterName.value;
+    console.log(clientName);
+
+    // if someone is creating a room, they are automatically a player
+    isStarter = true;
+
+    // send to hub
+    connection.server.newRoom({
+        name: clientName,
+        allowAudience: allowAudienceChoice
+    });
+}
+
+// Fetch an existing wait room
+function decideHowToEnter() {
+
+    // make sure a game code has been entered - and is valid
+    if (gameCodeInput.value != '') {
+        checkValidGameCode(gameCodeInput.value);
+    } else {
+        decideHowToEnterProceed();
+    }
+}
+
+// continuation of decideHowToEnter() that only gets called after important code is executed.
+function decideHowToEnterProceed() {
+    if (gameCodeInput.value === '') {
+        alert('Please enter a room code.');
+    } else if (!isValidGameCode) {
+        alert('That code is not valid.');
+    } else {
+        console.log("Entering room.");
+        clientName = enterName.value;
+        console.log(clientName);
+
+        // send to hub
+        connection.server.existingRoom({
+            name: clientName,
+            gameCode: gameCodeInput.value
+        });
+    }
+}
+
+function joinAsPlayer() {
+    console.log("joining as player");
+
+    // send to hub
+    connection.server.joinAsPlayer({
+        name: clientName,
+        setup: gameSetup
+    });
+}
+
+function joinAsWatcher() {
+    console.log("joining as watcher");
+
+    // send to hub
+    connection.server.joinAsWatcher({
+        name: clientName,
+        setup: gameSetup
+    });
+}
+
+function askAboutAudience() {
+
+    // first make sure a name has been entered
+    if (enterName.value != '') {
+
+        console.log("asking...");
+        allowAudienceModal.style.display = "block";
+    } else {
+        alert('Please enter a name.');
+    }
+}
+
+function askForGameCode() {
+
+    // first make sure a name has been entered
+    if (enterName.value != '') {
+
+        console.log("asking for code...");
+        gameCodeModal.style.display = "block";
+        initialGameCodeModalDisplay.style.display = "block";
+        joinOptionsDisplay.style.display = "none";
+    } else {
+        alert('Please enter a name.');
+    }
+}
+
+function getJoinRoomOptions() {
+
+    console.log("getting join options...");
+    initialGameCodeModalDisplay.style.display = "none";
+    joinOptionsDisplay.style.display = "block";
+    noSpaceAvailable.style.display = "block";
+
+    // determine which buttons to show
+    if (gameSetup.PlayersAvailableToJoin > 0) {
+        noSpaceAvailable.style.display = "none";
+        joinAsPlayerBtn.style.display = "block";
+    } else {
+        joinAsPlayerBtn.style.display = "none";
+    }
+
+    if (gameSetup.WatchersAvailableToJoin > 0) {
+        noSpaceAvailable.style.display = "none";
+        joinAsWatcherBtn.style.display = "block";
+    } else {
+        joinAsWatcherBtn.style.display = "none";
+    }
+}
+
+// check the hub to make sure the game code exists
+function checkValidGameCode(code) {
+    console.log('checking if game code is valid');
+    connection.server.isValidCode(code);
+}
+
+// return method for creating a room
+function enterWaitRoom() {
+    console.log("Entering wait room.");
+    // hide the initial div and show the wait room
+    intialOptions.style.display = "none";
+    gameCodeModal.style.display = "none";
+    waitRoom.style.display = "block";
+    document.getElementById('chatArea').style.display = "block";
+    roomCodeDisplay.innerHTML = gameSetup.GameCode;
+
+    // update wait room
+    updateWaitRoom();
+}
+
+// Update methods
+
+// update all the participants in a wait room - and anything else necessary
+function updateWaitRoom() {
+    updateEnterGameButton();
+    updateWaitRoomPlayers();
+    updateWaitRoomWatchers();
+}
+
+// update wait room player list
+function updateWaitRoomPlayers() {
+    console.log('updating wait room players');
+    playersAvailable.innerHTML = gameSetup.PlayersAvailableToJoin;
+    waitRoomPlayers.innerHTML = "";
+    // loop through list of players in the setup
+    for (var i = 0; i < gameSetup.Players.length; i++) {
+        waitRoomPlayers.innerHTML += gameSetup.Players[i].Name + "</br>";
+    }
+}
+
+// update wait room watcher list
+function updateWaitRoomWatchers() {
+    console.log('updating wait room watchers');
+    watchersAvailable.innerHTML = gameSetup.WatchersAvailableToJoin;
+    waitRoomWatchers.innerHTML = "";
+    // loop through list of players in the setup
+    for (var i = 0; i < gameSetup.Watchers.length; i++) {
+        waitRoomWatchers.innerHTML += gameSetup.Watchers[i].Name + "</br>";
+    }
+}
+
+// variables
+var connection;
+/*var gameSetup;*/
+/*var clientName;*/
+var isValidGameCode = false;
+
+var intialOptions = document.getElementById('initialOptions');
+var enterName = document.getElementById('enterName'); // TODO Add validation for name
+var newRoomBtn = document.getElementById('createRoomBtn');
+var joinRoomBtn = document.getElementById('joinRoomBtn');
+
+var allowAudienceModal = document.getElementById('allowAudienceModal');
+var yesAudienceBtn = document.getElementById('yesAudienceBtn');
+var noAudienceBtn = document.getElementById('noAudienceBtn');
+var audienceModalClose = document.getElementById('audienceModalClose');
+
+var gameCodeModal = document.getElementById('gameCodeModal');
+var initialGameCodeModalDisplay = document.getElementById('initialGameCodeModalDisplay');
+var gameCodeInput = document.getElementById('gameCodeInput');
+var submitGameCodeBtn = document.getElementById('submitGameCodeBtn');
+var codeModalClose = document.getElementById('codeModalClose');
+
+var joinOptionsDisplay = document.getElementById('joinOptionsDisplay');
+var joinAsPlayerBtn = document.getElementById('joinAsPlayerBtn');
+var joinAsWatcherBtn = document.getElementById('joinAsWatcherBtn');
+var noSpaceAvailable = document.getElementById('noSpaceAvailable');
+
+var waitRoom = document.getElementById('waitRoom');
+var roomCodeDisplay = document.getElementById('roomCode');
+var waitRoomPlayers = document.getElementById('waitRoomPlayers');
+var waitRoomWatchers = document.getElementById('waitRoomWatchers');
+var playersAvailable = document.getElementById('playersAvailable');
+var watchersAvailable = document.getElementById('watchersAvailable');
+
+var startGameBtn = document.getElementById('startGameBtn');
+
+// event handlers
+newRoomBtn.addEventListener("click", askAboutAudience);
+yesAudienceBtn.addEventListener("click", function () {
+    newRoom(true);
+});
+noAudienceBtn.addEventListener("click", function () {
+    newRoom(false);
+});
+
+joinRoomBtn.addEventListener("click", askForGameCode);
+submitGameCodeBtn.addEventListener("click", function () {
+    decideHowToEnter();
+});
+
+joinAsPlayerBtn.addEventListener("click", joinAsPlayer);
+joinAsWatcherBtn.addEventListener("click", joinAsWatcher);
+
+audienceModalClose.addEventListener("click", function () {
+    allowAudienceModal.style.display = "none";
+});
+codeModalClose.addEventListener("click", function () {
+    gameCodeModal.style.display = "none";
+});
+
+startGameBtn.addEventListener("click", goToGame);
+
--- SignalRGameSetup/Scripts/board.js
+++ SignalRGameSetup/Scripts/board.js
@@ -14,7 +14,7 @@ function getCoordinates(positionString) {
         col = parseInt(positionString.substring(2, positionString.length));
     }
 
-    let coordinates = { row, col };
+    let coordinates = [row, col];
     return coordinates;
 }
 
@@ -65,6 +65,8 @@ function addSquares(boardId) {
                 square.setAttribute('position', r + '-' + c);
                 square.addEventListener('click',
                     function () { clickSquare(square.getAttribute('position')); });
+                square.addEventListener('mouseover',
+                    function () { mouseOverSquare(square.getAttribute('position')); });
 
                 // TODO consider adding a highlight to the inside square
                 let pegHole = document.createElement('div');
@@ -95,7 +97,7 @@ function addSquares(boardId) {
 
     }
 
-    // Square clicking functions
+    // Square clicking/mouseover functions
     function clickSquare(positionString) {
         console.log('square clicked');
         if (isSettingShips && shipToSet != null) {
@@ -104,15 +106,251 @@ function addSquares(boardId) {
         // do other stuff if not setting ships
     }
 
+    function mouseOverSquare(positionString) {
+        console.log('mouse over');
+        let square = document.getElementById('square' + positionString);
+        let shipPreview = document.getElementById('preview' + positionString);
+        let positionCoordinates = getCoordinates(positionString);
+        // first make sure ships are being set and there's already a first position in the preview
+        if (isSettingShips && firstPosition.length != 0 && previewPositions.length != 0 &&
+            previousPosition.length != 0) {
+
+            // if it's in the line of square previews and has a preview, unset everything after it
+            if (isInPreviews(positionCoordinates) && square.getAttribute('hasPreview') == 'true') {
+                // figure out position of that coordinate in the array
+                let squareIndex = getPreviewIndex(positionCoordinates);
+                // loop and unset anything after that index in the array
+                previousPosition = positionCoordinates;
+                console.log('removing extra preview');
+                for (let i = squareIndex + 1; i < previewPositions.length; i++) {
+                    let preview = document.getElementById('preview' + previewPositions[i][0] +
+                        '-' + previewPositions[i][1]);
+                    preview.setAttribute('hasPreview', false);
+                    previewPositions.splice(i, 1);
+/*                    previewPositions = previewPositions[i].splice(i, 1);*/
+                    preview.className = 'ship-preview hide';
+                    squareCount--;
+                }
+
+                // if previous positions length is now only equal to one, set the direction to null
+                if (previewPositions.length == 1) {
+                    previewDirection = null;
+                }
+
+                //// now if the square in question is the same as the first square,
+                //// unset the direction
+                //if (positionCoordinates[0] === firstPosition[0] &&
+                //    positionCoordinates[1] === firstPosition[1]) {
+                //    previewDirection = null;
+                //}
+
+                // now check that this square is next to the previous square
+            } else if (isNextTo(positionCoordinates, previousPosition)) {
+
+                // check if it's the second position (check the count) - if it is, set the direction
+                if (squareCount === 1) {
+                    previewDirection = getDirection(positionCoordinates, previousPosition);
+                }
+
+                // check that it's going in the correct direction
+                if (previewDirection === getDirection(positionCoordinates, previousPosition)) {
+                    // if it is, then set the preview to visible and add to count - determine color
+                    square.setAttribute('hasPreview', true);
+                    previewPositions.push(positionCoordinates);
+                    squareCount++;
+
+                    // see if it's the final square
+                    if (isFinalPreviewSquare(positionCoordinates)) {
+                        shipPreview.className = 'ship-preview last';
+                    } else if (isBeyondFinalPreviewSquare(positionCoordinates)) { // if it's beyond the final square, show an error
+                        shipPreview.className = 'ship-preview error';
+                    } else { // otherwise set it to preview
+                        shipPreview.className = 'ship-preview';
+                    }
+
+                }
+
+            }
+
+
+        }
+        // check if it has
+
+    }
+
+    // check if it should be the last position in a direction
+    function isFinalPreviewSquare(coordinates) {
+        console.log('Is it the last preview square?');
+        let result = false;
+        // figure out the final preview square - if fields are not blank
+        if (isSettingShips && firstPosition.length != 0 &&
+            previewDirection != null && currentShip != null) {
+            let position = getFinalPreviewSquare();
+            if (position[0] === coordinates[0] &&
+                position[1] === coordinates[1]) {
+                result = true;
+            }
+        }
+
+        // return result
+        console.log(result);
+        return result;
+    }
+
+    function isBeyondFinalPreviewSquare(coordinates) {
+        console.log('seeing if its beyound the final preview square');
+        let result = false;
+        // figure out the final preview square - if fields are not blank
+        if (isSettingShips && firstPosition.length != 0 &&
+            previewDirection != null && currentShip != null) {
+            let finalPosition = getFinalPreviewSquare();
+            // determine final based on ship length and direction
+            if (previewDirection === "north") {
+                if (coordinates[0] > finalPosition[0] &&
+                    coordinates[1] === finalPosition[1]) {
+                    result = true;
+                }
+            } else if (previewDirection === "south") {
+                if (coordinates[0] < finalPosition[0] &&
+                    coordinates[1] === finalPosition[1]) {
+                    result = true;
+                }
+            } else if (previewDirection === "west") {
+                if (coordinates[0] === finalPosition[0] &&
+                    coordinates[1] > finalPosition[1]) {
+                    result = true;
+                }
+            } else if (previewDirection === "east") {
+                if (coordinates[0] === finalPosition[0] &&
+                    coordinates[1] < finalPosition[1]) {
+                    result = true;
+                }
+            }
+        }
+        console.log(result);
+        return result;
+    }
+
+    function getFinalPreviewSquare() {
+        console.log('settng final preview square');
+        let position = new Array();
+        let lengthToAccount = currentShip.Length - 1;
+        // figure out the final preview square - if fields are not blank
+        if (isSettingShips && firstPosition.length != 0 &&
+            previewDirection != null && currentShip != null) {
+            // determine final based on ship length and direction
+
+            if (previewDirection === "north") {
+                position = [firstPosition[0] + lengthToAccount, firstPosition[1]];
+            } else if (previewDirection === "south") {
+                position = [firstPosition[0] - lengthToAccount, firstPosition[1]];
+            } else if (previewDirection === "west") {
+                position = [firstPosition[0], firstPosition[1] + lengthToAccount];
+            } else if (previewDirection === "east") {
+                position = [firstPosition[0], firstPosition[1] - lengthToAccount];
+            }
+        }
+        console.log(position);
+        return position;
+    }
+
+    // check if a position is in a row of highlighted cells
+    function isInPreviews(coordinates) {
+        console.log('checking if it exists in previews');
+        if (previewPositions.length === 0) {
+            console.log('false - no positions set');
+            return false;
+        }
+        for (var i = previewPositions.length - 1; i >= 0; i--)
+        {
+            if (coordinates[0] === previewPositions[i][0] &&
+                coordinates[1] === previewPositions[i][1]) {
+                console.log('true');
+                return true;
+            }
+        }
+        console.log('false');
+        return false;
+    }
+
+    // get the index of a position in the list of preview cells by a coordinate
+    function getPreviewIndex(coordinates) {
+        console.log('getting preview index');
+        if (previewPositions.length === 0) {
+            console.log('error');
+            return -1; // -1 indicates error
+        }
+        for (var i = previewPositions.length - 1; i >= 0; i--) {
+            if (coordinates[0] === previewPositions[i][0] &&
+                coordinates[1] === previewPositions[i][1]) {
+                console.log('index: ' + i);
+                return i;
+            }
+        }
+        console.log('error');
+        return -1;
+    }
+
+    // see if a square is next to another
+    function isNextTo(coordinates, previous) {
+        console.log('Is next to previous?');
+        console.log('coordinates: ' + coordinates[0] + ', ' + coordinates[1]);
+        console.log('previous: ' + previous);
+        let north = [previous[0] - 1, previous[1]];
+        let south = [previous[0] + 1, previous[1]]
+        let west = [previous[0], previous[1] - 1];
+        let east = [previous[0], previous[1] + 1];
+        console.log('North: ' + north);
+        console.log('South: ' + south);
+        console.log('West: ' + west);
+        console.log('East: ' + east);
+
+        if ((coordinates[0] === north[0] && coordinates[1] === north[1]) ||
+            (coordinates[0] === south[0] && coordinates[1] === south[1]) || 
+            (coordinates[0] === west[0] && coordinates[1] === west[1]) || 
+            (coordinates[0] === east[0] && coordinates[1] === east[1])) {
+            console.log('true');
+            return true;
+        } else {
+            console.log('false');
+            return false;
+        }
+
+    }
+
+    // determine which direction a line of squares is going
+    function getDirection(coordinates, previous) {
+        console.log('What direction?');
+        let north = [(previous[0] + 1), (previous[1])];
+        let south = [previous[0] - 1, previous[1]]
+        let west = [previous[0], previous[1] + 1];
+        let east = [previous[0], previous[1] - 1];
+        if (coordinates[0] === north[0] && coordinates[1] === north[1]) {
+            console.log('north');
+            return 'north';
+        } else if (coordinates[0] === south[0] && coordinates[1] === south[1]) {
+            console.log('south');
+            return 'south';
+        } else if (coordinates[0] === west[0] && coordinates[1] === west[1]) {
+            console.log('west');
+            return 'west';
+        } else if (coordinates[0] === east[0] && coordinates[1] === east[1]) {
+            console.log('east');
+            return 'east';
+        } else {
+            return null;
+        }
+    }
+
     // setting ship functions
     function setShipPosition(positionString) {
         console.log('setting position');
         let square = document.getElementById('square' + positionString);
         let shipPreview = document.getElementById('preview' + positionString);
         let positionCoordinates = getCoordinates(positionString);
-        console.log('Square id: ' + square.id);
-        console.log('has ship: ' + square.getAttribute('hasShip'));
-        console.log('has peg: ' + square.getAttribute('hasPeg'));
+        //console.log('Square id: ' + square.id);
+        //console.log('has ship: ' + square.getAttribute('hasShip'));
+        //console.log('has peg: ' + square.getAttribute('hasPeg'));
 
         // first make sure it doesn't have a ship or peg
         if (square.getAttribute('hasShip') == 'false' &&
@@ -130,14 +368,18 @@ function addSquares(boardId) {
                 shipPreview.className = 'ship-preview hide';
                 firstPosition.length = 0; // like setting them null
                 previousPosition.length = 0; // like setting it to null
+                previewPositions.length = 0;
                 square.setAttribute('hasPreview', false);
+                squareCount = 0;
 
             } else if (firstPosition.length === 0) { // otherwise if a position is not set yet
                 console.log('setting preview');
                 firstPosition = positionCoordinates;
                 previousPosition = positionCoordinates;
+                previewPositions.push(positionCoordinates);
                 shipPreview.className = 'ship-preview';
                 square.setAttribute('hasPreview', true);
+                squareCount = 1;
 
             } else { // otherwise if it's the last position that can be set - set the ship!
 
@@ -179,16 +421,20 @@ class TestPositionClass {
     }
 }
 
+
 var testShip = new TestShipClass('Carrier', 5);
 
 // ship setting variables
 // HACK First two variables are set temporarily to create functions
 var isSettingShips = true; // default = false
 var shipToSet = testShip; // this should be null unless ships are being set
-var firstPosition = []; // null indicates nothing is clicked
-var previousPosition = []; // preview position that mouse dragged over
-var lastPosition = []; // should stay null until last position is clicked
-var setDirection = null;
+var squareCount = 0; // how many squares have been dragged over
+var firstPosition = new Array(); // null indicates nothing is clicked
+var previousPosition = new Array(); // preview position that mouse dragged over
+var finalPosition = new Array(); // should stay null until last position is clicked
+var previewPositions = new Array(); // all positions in the current stream of previews lol
+var previewDirection = null;
+var currentShip = testShip; // HACK for now it is set to the test ship
 
 
 
